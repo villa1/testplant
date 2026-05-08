3aec455a6c0c9a3a37447946d1399909ca388545
villa puncak <sewavilladipuncak.net@gmail.com>
2026-05-08 08:57:25 +0700

    Revert "feat: add initial payload migrations for Vercel"
    This reverts commit 4f784fb23261e9187085eff42f9809d6ac4ff7ca.

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260508_013151 from './20260508_013151';
-
-export const migrations = [
-  {
-    up: migration_20260508_013151.up,
-    down: migration_20260508_013151.down,
-    name: '20260508_013151'
-  },
-];
