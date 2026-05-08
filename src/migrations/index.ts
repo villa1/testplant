4f784fb23261e9187085eff42f9809d6ac4ff7ca
villa puncak <sewavilladipuncak.net@gmail.com>
2026-05-08 08:33:56 +0700

    feat: add initial payload migrations for Vercel

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260508_013151 from './20260508_013151';
+
+export const migrations = [
+  {
+    up: migration_20260508_013151.up,
+    down: migration_20260508_013151.down,
+    name: '20260508_013151'
+  },
+];
